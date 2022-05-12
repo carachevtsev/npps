635ff04323ca62a2be73b854eed04c910b61c18d
carachevtsev <alex.carachevtsev@gmail.com>
2022-05-12 18:05:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-l4nk2d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-l4nk2d)
---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+/*var n = prompt("Введите значение n", "");
+var q = prompt("Введите значение Q", "");
+var s
+var result = 0
+let arr = [];
+for (var i =0; i<n; i+=1){
+document.write(i);
+document.write("<br />");
+s= q**(i+1);
+result= result + s
+
+/*arr = arr.filter(function(n){return n; });*/
+/*}
+console.log(result)
+
+console.log(arr);
+document.write(arr);*/
+ 
+
+
+ 
